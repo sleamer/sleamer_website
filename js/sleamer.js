6059366dc9582ce33186b4897855b60809ba6b30
sleamer <sleamer@hotmail.com>
2014-08-12 12:44:18 -0700

    Carousel in art.html working. minor style fixes

--- js/sleamer.js
+++ js/sleamer.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
-		$('a').click().smoothScroll({offset: -100});
-		$('#artCarousel').slick({
-          	dots: true,
-  			infinite: true,
-  			speed: 500,
+      $('a').click().smoothScroll({offset: -100});
+      var $carousel = $('#artCarousel');
+      $carousel.slick({
+      slidesToShow: 1,
+      slidesToScroll: 1
       });
-});	
+
+      $('.js-filter').on('click', function() {
+          // Undo any previous filtering
+          $carousel.slickUnfilter();
+          $('li').removeClass('active');
+          $(this).parent().addClass('active');
+          var navArtSubject = $(this).data('art-subject');
+          if (navArtSubject !== 'all') {
+              $carousel.slickFilter('.artImage[data-art-subject="' + navArtSubject + '"]');
+          }
+      });
+
+});
